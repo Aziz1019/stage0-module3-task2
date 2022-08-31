8feecc7638f7407d50f8c389d1898413dd149181
Aziz1019 <starinc1998@gmail.com>
2022-08-31 15:16:20 +0500

    Initial Commit

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+    private static final int adultAge = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
 
-    public void CAllToFriend() {
+    public void callToFriend() {
         //some code
     }
 
-    public void callByNumber(int Number) {
+    public void callByNumber(int number) {
         //some code
     }
 }
